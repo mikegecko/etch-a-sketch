8cbcaabd5b1537c69474732c87d8efd019020837
Michael Guedko <mjgeko@gmail.com>
2022-02-14 17:32:07 -0500

    Button stuff

--- scripts.js
+++ scripts.js
@@ -1,4 +1,11 @@
-//Add event handlers
+const btnClear = document.querySelector('#clear');
+const btnErase = document.querySelector('#erase');;
+const btnColor = document.querySelector('#colormd');;
+const btnRainbow = document.querySelector('#rainbowmd');;
+
+//Adding event handlers
+
+
 
 //Add grid components
 
