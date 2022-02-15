489e092707085428ef04057ad7bfa712e3f02f77
Michael Guedko <mjgeko@gmail.com>
2022-02-15 13:18:54 -0500

    Setup grid creation

--- scripts.js
+++ scripts.js
@@ -1,15 +1,31 @@
 const buttons = document.querySelectorAll('.btn');
 const slider = document.querySelector('.slider');
 const sliderOutput = document.querySelector('.slider-value');
+const gridContainer = document.querySelector('.grid-container')
 
 //Adding event handlers
 slider.addEventListener('input', sliderHandler);
 buttons.forEach(element => {
     element.addEventListener('click', btnHandler)
 });
+
+//Add grid components
+createGrid(slider.value);
+
+
+//Add functions for clear|erase|colormode|rainbowmode|colorpicker
+function clearGrid(){
+
+}
+function eraseGrid(){
+
+}
+
 function sliderHandler(event){
     sliderOutput.innerHTML = slider.value + " x " + slider.value;
+    createGrid(slider.value);
 }
+
 function btnHandler(event){
     //send each event to proper functions
     switch(event.target.id){
@@ -30,12 +46,11 @@ function btnHandler(event){
     }
 }
 
-//Add grid components
-
-//Add functions for clear|erase|colormode|rainbowmode|colorpicker
-function clearGrid(){
-
-}
-function eraseGrid(){
-
+function createGrid(gridSize){
+    gridContainer.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+    for(let i = 0; i < gridSize ** 2 ; i++){
+        let newDiv = document.createElement('div');
+        newDiv.style.backgroundColor = "white";
+        gridContainer.appendChild(newDiv);
+    }
 }
